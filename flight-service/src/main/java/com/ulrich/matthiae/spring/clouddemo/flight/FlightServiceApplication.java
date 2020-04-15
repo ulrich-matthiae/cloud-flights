85e80ce1ce6047afffae6891d51da1afea24315a
silentfred <cmptrwizard@gmail.com>
2020-04-15 14:29:46 +0200

    Additional tweaks, fixes and cleanups

--- flight-service/src/main/java/com/ulrich/matthiae/spring/clouddemo/flight/FlightServiceApplication.java
+++ flight-service/src/main/java/com/ulrich/matthiae/spring/clouddemo/flight/FlightServiceApplication.java
@@ -2,7 +2,6 @@ package com.ulrich.matthiae.spring.clouddemo.flight;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.openfeign.EnableFeignClients;
@@ -10,7 +9,6 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @SpringBootApplication
 @EnableFeignClients
 @EnableDiscoveryClient
-@EnableCircuitBreaker
 @EnableHystrix
 public class FlightServiceApplication {
 
