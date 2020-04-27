b5abf1ea13630f3a78362bf265eec5c7cdcd9c52
Ulrich Matthiae <ulrich.matthiae@entelect.co.za>
2020-04-27 15:15:08 +0200

    Cleanup

--- booking-service/src/main/java/com/ulrich/matthiae/spring/clouddemo/booking/client/flight/FlightServiceClient.java
+++ booking-service/src/main/java/com/ulrich/matthiae/spring/clouddemo/booking/client/flight/FlightServiceClient.java
@@ -1,12 +1,10 @@
 package com.ulrich.matthiae.spring.clouddemo.booking.client.flight;
 
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 @FeignClient(name = "flight-service")
-@Repository
 public interface FlightServiceClient {
 
     @GetMapping("/flights/{id}")
